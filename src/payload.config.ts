72a5851b452127f498881e4c66900b7bcbc1533f
Leonardo <leonardo.yakub@gmail.com>
2024-10-17 23:01:10 +0200

    project setup

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,13 +1,14 @@
 // storage-adapter-import-placeholder
 import { sqliteAdapter } from '@payloadcms/db-sqlite'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { uploadthingStorage } from '@payloadcms/storage-uploadthing'
 import path from 'path'
 import { buildConfig } from 'payload'
-import { fileURLToPath } from 'url'
 import sharp from 'sharp'
+import { fileURLToPath } from 'url'
 
-import { Users } from './collections/Users'
 import { Media } from './collections/Media'
+import { Users } from './collections/Users'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -28,10 +29,19 @@ export default buildConfig({
   db: sqliteAdapter({
     client: {
       url: process.env.DATABASE_URI || '',
+      authToken: process.env.DATABASE_AUTH_TOKEN,
     },
   }),
   sharp,
   plugins: [
-    // storage-adapter-placeholder
+    uploadthingStorage({
+      collections: {
+        [Media.slug]: true,
+      },
+      options: {
+        apiKey: process.env.UPLOADTHING_SECRET,
+        acl: 'public-read',
+      },
+    }),
   ],
 })
