e22e498f86427c066968ffe8e39e79cd152618d9
Leonardo <leonardo.yakub@gmail.com>
2024-10-31 16:43:34 +0100

    adding lucide-react to the transpilePackages option in next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
-import { withPayload } from '@payloadcms/next/withPayload'
+import { withPayload } from "@payloadcms/next/withPayload";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
-}
+  transpilePackages: ["lucide-react"],
+};
 
-export default withPayload(nextConfig)
+export default withPayload(nextConfig);
