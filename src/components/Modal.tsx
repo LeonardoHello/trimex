241fd5c4908a79388f6394adbfd1fbbc3c01c3f4
Leonardo <leonardo.yakub@gmail.com>
2024-11-12 05:17:03 +0100

    fixing modal background cancel

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -21,11 +21,10 @@ export default function Modal({ children }: { children: React.ReactNode }) {
       }}
       className="fixed inset-0 z-50 grid place-content-center bg-background/85 duration-300 animate-in fade-in"
     >
-      <div
-        className="overflow-y-scroll py-20"
-        onClick={(e) => e.stopPropagation()}
-      >
-        {children}
+      <div className="overflow-y-scroll">
+        <div className="my-20" onClick={(e) => e.stopPropagation()}>
+          {children}
+        </div>
       </div>
     </div>
   );
