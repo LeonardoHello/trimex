ff8ed5fc4ed9132a0d9fd58696af6ca79f3b3693
Leonardo <leonardo.yakub@gmail.com>
2024-12-04 12:35:24 +0100

    removed unused components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -62,9 +62,6 @@ const config: Config = {
       },
       animation: {
         pulse: "pulse var(--duration) ease-out infinite",
-        "shimmer-slide":
-          "shimmer-slide var(--speed) ease-in-out infinite alternate",
-        "spin-around": "spin-around calc(var(--speed) * 2) infinite linear",
         rainbow: "rainbow var(--speed, 2s) infinite linear",
       },
       keyframes: {
@@ -74,25 +71,6 @@ const config: Config = {
           },
           "50%": {
             boxShadow: "0 0 0 8px var(--pulse-color)",
-          },
-        },
-        "shimmer-slide": {
-          to: {
-            transform: "translate(calc(100cqw - 100%), 0)",
-          },
-        },
-        "spin-around": {
-          "0%": {
-            transform: "translateZ(0) rotate(0)",
-          },
-          "15%, 35%": {
-            transform: "translateZ(0) rotate(90deg)",
-          },
-          "65%, 85%": {
-            transform: "translateZ(0) rotate(270deg)",
-          },
-          "100%": {
-            transform: "translateZ(0) rotate(360deg)",
           },
         },
         rainbow: {
